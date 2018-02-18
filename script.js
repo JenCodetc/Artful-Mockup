81c5192a9814e4918b18fb3341d8f31b45c132d7
Jennifer Li <brnli7@gmail.com>
2018-02-18 16:48:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function likes() {
+    document.getElementById('likes').src = "https://png.icons8.com/ios/96/e74c3c/like-filled.png";
+}
+
+function likes1() {
+    document.getElementById('likes1').src = "https://png.icons8.com/ios/96/e74c3c/like-filled.png";
+}
+
+function likes2() {
+    document.getElementById('likes2').src = "https://png.icons8.com/ios/96/e74c3c/like-filled.png";
+}
+
